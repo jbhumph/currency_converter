b652e699da0daa73649611874c5cb9a34eb6e1db
John <jbhumph@gmail.com>
2024-11-16 22:15:20 -0800

    Finished primary logic.

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 // add items from page
+const amount = document.querySelector("#amount");
+const from = document.querySelector("#from");
+const to = document.querySelector("#to");
+const swap = document.querySelector("#swap_button");
+const submit = document.querySelector(".convert");
+
+const resp_init = document.querySelector("#resp_init");
+const resp_total = document.querySelector("#resp_total");
+const resp_other = document.querySelector("#resp_other");
+
+const convert_boxes = document.querySelector(".convert_boxes");
 
 //button listener
+swap.addEventListener("click", () => {
+    let temp = from.value;
+    from.value = to.value;
+    to.value = temp;
+})
+
+submit.addEventListener("click", () => {
+    if (isNaN(amount.value)) {
+        alert("Please enter a valid number");
+    } else {
+        convert_boxes.innerHTML = "";
+        drawResponse();
+        drawBox(to.value, from.value);
+        drawBox(from.value, to.value);
+    }
+
+})
 
 const exchangeRates = {
     "base": 1,
@@ -15,12 +43,68 @@ const exchangeRates = {
         "INR": 81.255504,
         "JPY": 143.376504,
         "RUB": 57.875038,
+        "USD": 1.0,
         "ZAR": 17.92624
     }
 }
 
 // validate number input
+function validate() {
+
+}
 
 // convert function
+function convert(amt, init, end) {
+    return (amt / exchangeRates.rates[init]) * exchangeRates.rates[end];
+}
 
 // draw boxes
+function drawResponse() {
+    resp_init.innerText = `${amount.value} ${from.value} =`;
+    resp_total.innerText = `${convert(amount.value, from.value, to.value).toFixed(6)} ${to.value}`;
+    resp_other.innerText = `1 ${to.value} = ${convert(1, to.value, from.value).toFixed(6)} ${from.value}`;
+}
+
+function drawBox(curA, curB) {
+    const convert_box = document.createElement("div")
+    convert_box.classList.add("convert_box");
+
+    const box_title = document.createElement("div")
+    box_title.classList.add("box_title");
+    convert_box.append(box_title);
+    const bTitle = document.createElement("div")
+    bTitle.classList.add("bTitle");
+    bTitle.innerText = `Convert ${curA} to ${curB}`
+    box_title.appendChild(bTitle);
+
+    const box_heads = document.createElement("div")
+    box_heads.classList.add("box_heads");
+    convert_box.append(box_heads);
+    const box_headA = document.createElement("div")
+    box_headA.classList.add("box_head");
+    box_headA.innerText = `${curA}`;
+    const box_headB = document.createElement("div")
+    box_headB.classList.add("box_head");
+    box_headB.innerHTML = `${curB}`;
+    box_heads.appendChild(box_headA);
+    box_heads.appendChild(box_headB);
+
+    let numbers = [1, 5, 10, 25, 50, 100, 500, 1000, 5000, 10000];
+
+    for (let i = 0; i < 10; i++) {
+        const box_line = document.createElement("div")
+        box_line.classList.add("box_line");
+        const box_itemA = document.createElement("div")
+        box_itemA.classList.add("box_item");
+        box_itemA.innerText = `${numbers[i]} ${curA}`;
+        const box_itemB = document.createElement("div")
+        box_itemB.classList.add("box_item");
+        box_itemB.innerHTML = `${convert(numbers[i], curA, curB).toFixed(2)} ${curB}`;
+        box_line.append(box_itemA);
+        box_line.append(box_itemB);
+        convert_box.append(box_line);
+    }
+
+
+    convert_boxes.appendChild(convert_box);
+}
