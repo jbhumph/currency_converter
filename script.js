8c1b121752dcd5f93e037b7031c72fac5fd98f94
John <jbhumph@gmail.com>
2024-11-16 20:03:43 -0800

    Reworked Layout and imported images .

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
-const startValue = document.querySelector("#start");
-const endValue = document.querySelector("#end");
-const amountValue = document.querySelector("#amount");
-const submit = document.querySelector("#submit");
-const submitB = document.querySelector("#submitLookup");
-const look = document.querySelector("#lookup");
-const result = document.querySelector(".result");
-const lookupResult = document.querySelector(".lookupResult");
+// add items from page
 
-submit.addEventListener("click", (e) => {
-    e.preventDefault();
-    convert(startValue.value.toUpperCase(), endValue.value.toUpperCase(), amountValue.value);
-})
-
-submitB.addEventListener("click", (e) => {
-    e.preventDefault();
-    lookup(look.value)
-})
+//button listener
 
 const exchangeRates = {
     "base": 1,
@@ -34,71 +19,8 @@ const exchangeRates = {
     }
 }
 
-const knownSymbols = {
-    "symbols": {
-        "AUD": "Australian Dollar",
-        "CAD": "Canadian Dollar",
-        "CLP": "Chilean Peso",
-        "CNY": "Chinese Yuan",
-        "EUR": "Euro",
-        "GBP": "British Pound Sterling",
-        "INR": "Indian Rupee",
-        "JPY": "Japanese Yen",
-        "RUB": "Russian Ruble",
-        "USD": "United States Dollar",
-        "ZAR": "South African Rand"
-    },
-    "countries": {
-        "australia": "AUD",
-        "canada": "CAD",
-        "chile": "CLP",
-        "china": "CNY",
-        "euro": "EUR",
-        "britain": "GBP",
-        "india": "INR",
-        "japan": "JPY",
-        "russia": "Ruble",
-        "united states": "USD",
-        "south africa": "ZAR"
-    }
-}
+// validate number input
 
-function convert(start, end, value) {
-    // converts currency and updates DOM
-    let validated = validate(start, end, value);
-    if (!validated) {
-        result.textContent = "Invalid input. Please try again.";
-    }
-    let finalValue = (value / exchangeRates.rates[start]) * exchangeRates.rates[end];
-    result.textContent = `${value} ${start} = ${finalValue.toFixed(2)} ${end}`;
-    startValue.value = "";
-    endValue.value = "";
-    amountValue.value = "";
-}
+// convert function
 
-function validate(start, end, value) {
-    // function verifies proper input
-    if (exchangeRates.rates.hasOwnProperty(start) === false) {
-        alert("Please enter valid starting currency");
-        return false;
-    } else if (exchangeRates.rates.hasOwnProperty(end) === false) {
-        alert("Please enter valid ending currency");
-        return false;
-    } else if (isNaN(value)) {
-        alert("Please enter a valid number");
-        return false;
-    } else {
-        return true;
-    }
-}
-
-function lookup(input) {
-    // checks against known symbols object and updates DOM
-    if (knownSymbols.symbols.hasOwnProperty(input.toUpperCase())) {
-        lookupResult.textContent = `${input.toUpperCase()} = ${knownSymbols.symbols[input.toUpperCase()]}`;
-    } else if (knownSymbols.countries.hasOwnProperty(input.toLowerCase())) {
-        lookupResult.textContent = `${input.toLowerCase()} = ${knownSymbols.countries[input.toLowerCase()]}`;
-    } else {
-        lookupResult.textContent = "No results found.";
-    }
-}
+// draw boxes
